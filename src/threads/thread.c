a6bbb3b0ff0b6f0d9dc347952a44ed6aeb6c2d52
Min-Uk.Lee <makerdark98@gmail.com>
2019-09-03 22:12:09 +0900

    Solve Priority Change Test

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -213,6 +213,7 @@ thread_create (const char *name, int priority,
 
   /* Add to run queue. */
   thread_unblock (t);
+  thread_yield ();
 
   return tid;
 }
@@ -376,6 +377,7 @@ void
 thread_set_priority (int new_priority) 
 {
   thread_current ()->priority = new_priority;
+  thread_yield ();
 }
 
 /* Returns the current thread's priority. */
